0b27bb4319e9a1e4e883dd903a7746bbc51b7683
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-10-10 16:02:30 +0300

    Add banner component

---
+++ src/Components/Banner.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import React from 'react'
+import { Container, Row, Col} from 'react-bootstrap';
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import headerImg from "../assets/images/headerImg.svg"
+
+const Banner = () => {
+
+
+  return (
+    <section className='banner' id="home">
+        <Container>
+            <Row className="align-items-center">
+                <Col xs={12} md={6} xl={7}>
+                    <span className='tagline'>Welcome to my Portfolio</span>
+                    <h1>{`Hi I'm Peter`}<span className='wrap'>web developer</span></h1>
+                    <p> Lorem Ipsum</p>
+                    <button onClick={() => console.log("connect")}>Let's connect</button>
+                </Col>
+                <Col xs={12} md={6} xl={5}>
+                  <img src={headerImg} alt="Header Img"/>
+                  </Col>
+            </Row>
+        </Container>
+
+    </section>
+  )
+}
+
+export default Banner
