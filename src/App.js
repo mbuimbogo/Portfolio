7906bf7cf2b88aa0d6936101941072b4ae62e592
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-10-11 00:40:53 +0300

    Add skills component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import NavBar from './Components/NavBar';
 import Banner from './Components/Banner';
+import Skills from './Components/Skills';
 import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
      <NavBar/>
      <Banner/>
+     <Skills/>
     </div>
   );
 }
