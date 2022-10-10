ce51d4ddfc3c6b3561c43c43bfaf6a878446abf8
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-10-10 16:08:49 +0300

    Add rotate array

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -7,7 +7,7 @@ import headerImg from "../assets/images/headerImg.svg"
 const Banner = () => {
   const [loopNum, setLoopNum]=useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
-  
+  const toRotate = ["Web Developer", "Web Designer","UI/UX Designer"];
   const [text, setText] = useState("");
   const [delta, setDelta] = useState(300-Math.random()*100);
 
