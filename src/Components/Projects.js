ef5fbad53dcaf924b57cbd65b5d3abc0f9ec92ef
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-10-11 01:56:38 +0300

    Add Projects' Component

---
+++ src/Components/Projects.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import { Nav } from 'react-bootstrap'
+import { Tab } from 'react-bootstrap'
+import {Col, Container, Row} from 'react-bootstrap'
+
+const Projects = () => {
+    const projects = [
+        {
+            title: "Business Startup",
+            description: 'Design & Development',
+            imgUrl: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: 'Design & Development',
+            imgUrl: projImg2,
+        },
+        {
+            title: "Business Startup",
+            description: 'Design & Development',
+            imgUrl: projImg3,
+        },
+        {
+            title: "Business Startup",
+            description: 'Design & Development',
+            imgUrl: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: 'Design & Development',
+            imgUrl: projImg1,
+        }
+    ]
+  return (
+    <section className='project' id="project">
+        <Container>
+            <Row>
+                <Col>
+                <h2>Projects</h2>
+                <p>Lorem Ipsum</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey={first}>
+                <Nav variant="pills" defaultActiveKey="/home">
+                <Nav.Item>
+                <Nav.Link eventKey="first">Tab One</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                <Nav.Link eventKey="third">
+                    Tab Three
+                </Nav.Link>
+                </Nav.Item>
+                </Nav>
+                <Tab.Content>
+                    <Tab.Pane eventKey="first">
+                    <Row>
+                    {projects.map((project,index) => {
+                        return(
+                            <p>{project.title}</p>
+                        )
+                    })}
+                    </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">Loreen Ipsun</Tab.Pane>
+                    <Tab.Pane eventKey="third">Loreen Ipsun</Tab.Pane>
+                </Tab.Content>
+                </Tab.Container>
+                </Col>
+            </Row>
+        </Container>
+    </section>
+  )
+}
+
+export default Projects
