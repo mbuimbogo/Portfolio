ecddff5da37e107ba8a4514e60eb774759449537
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-10-13 00:07:28 +0300

    Setstate in contact component

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Contact = () => {
+    const formInitialDetails = {
+        firstName: "",
+        lastName:"",
+        email:"",
+        phone: "",
+        message:"",
+    }
+
+    const [formDetails, setFormDetails] = useState(formInitialDetails);
+    const [buttonText, setButtonText] = useState("send");
+    const [status, setStatus] = useState({}) //checking errors
   return (
     <div>Contact</div>
   )
