30b4ec8d2833d9fe0ba3deeac0b5c57eb4233902
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-10-13 02:32:43 +0300

    Update footer compontent

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import logo from "../assets/images/logo.svg"
+import navIcon1 from "../assets/images/nav-icon1.svg";
+import navIcon2 from "../assets/images/nav-icon2.svg";
+import navIcon3 from "../assets/images/nav-icon3.svg";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='footer'>
+    <Container>
+    <Row className="align-item-center">
+    <Col sm = {6}>
+        <img src={logo} alt="Logo" />
+    </Col>
+    <Col sm={6} className= "text-center text-sm-end">
+        <div className='social-icon'>
+            <a href="">img src={navIcon1}</a>
+            <a href="">img src={navIcon2}</a>
+            <a href="">img src={navIcon3}</a>
+        </div>
+        <p>CopyRight 2022. All Rights Reserved by mbogombui</p>
+    </Col>
+    </Row>
+    </Container>
+    </footer>
   )
 }
 
